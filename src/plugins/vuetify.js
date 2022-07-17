2cf72c10749e721ba6a736dc2d0ab2ec18d7aa8f
alsoson <0971579957lulu@gmail.com>
2022-07-17 23:17:37 +0800

    first commit

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,38 @@
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+// Vuetify
+import { createVuetify } from 'vuetify'
+
+export default createVuetify(
+  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+  {
+    theme: {
+      defaultTheme: 'dark'
+    }
+  }
+)
+
+// 顏色
+// #009cb8
+// #ffa899
+// #ff6347 -初始
+// #ff5330
+// #ed7059
+// #ff2c06
+// #ff0000
+// #a3a3a3
+// #22c32e
+// #e6cfe6
+
+// ---
+
+// #d11f47
+// #fe4370
+// #ff667a
+// #55a6a1
+// #809698
+// #ea315a
+// #FE6579
+// #AA868E
---
+++ src/views/HomeView.vue
@@ -0,0 +1,79 @@
+<template lang="pug" >
+v-row.text-center#home
+  v-col(cols="12")
+    | 1234
+  v-col(cols="12")
+    h1 {{ timeText }}
+    v-btn.mx-3(icon color="#55a6a1" v-if="status !== 1" @click="startTimer")
+      v-icon mdi-play
+    v-btn.mx-3(icon color="#55a6a1" v-else @click="pauseTimer")
+      v-icon mdi-pause
+    v-btn.mx-3(icon color="#fe4370" v-if="current.length > 0" @click="finishTimer(true)")
+      v-icon mdi-skip-next
+  v-col(cols="12")
+    h1 {{ currentText }}
+</template>
+
+<script setup>
+import { computed, ref } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useListStore } from '@/stores/list'
+import { useSettingsStore } from '@/stores/settings'
+
+const list = useListStore()
+const { countdown, start, finish } = list
+
+const settings = useSettingsStore()
+const { current, items, timeleft } = storeToRefs(list)
+const { selectedAlarmFile } = storeToRefs(settings)
+
+const currentText = computed(() => {
+  return current.value.length > 0 ? current.value : items.value.length > 0 ? '點擊開始' : '沒有事項'
+})
+const timeText = computed(() => {
+  const m = Math.floor(timeleft.value / 60).toString().padStart(2, '0')
+  const s = (timeleft.value % 60).toString().padStart(2, '0')
+  return m + ':' + s
+})
+
+let timer
+// 0 = 停止
+// 1 = 倒數中
+// 2 = 暫停
+const status = ref(0)
+
+const pauseTimer = () => {
+  status.value = 2
+  clearInterval(timer)
+}
+
+const finishTimer = (skip) => {
+  clearInterval(timer)
+  status.value = 0
+  finish()
+  if (!skip) {
+    const audio = new Audio()
+    audio.src = selectedAlarmFile.value
+    audio.play()
+  }
+  if (items.value.length > 0) {
+    startTimer()
+  }
+}
+
+const startTimer = () => {
+  if (status.value === 0 && items.value.length > 0) {
+    start()
+  }
+  if (current.value.length > 0) {
+    status.value = 1
+    timer = setInterval(() => {
+      countdown()
+      if (timeleft.value === 0) {
+        finishTimer(false)
+      }
+    }, 1000)
+  }
+}
+
+</script>
