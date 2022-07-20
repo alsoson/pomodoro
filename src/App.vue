203dda4cbf8b7a5d9f76c5c520f6141c63c45158
alsoson <0971579957lulu@gmail.com>
2022-07-20 16:05:43 +0800

    先醬吧

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,15 @@ v-app
       //-v-img(src='https://cdn-icons-png.flaticon.com/512/7329/7329721.png')
       h3.d-inline-block.ms-3 番茄鐘
     v-spacer
-    v-btn(icon to='/')
+    v-btn(icon, to="/")
       v-icon(large) mdi-timer
-    v-btn(icon to='/list')
+    v-btn(icon, to="/list")
       v-icon mdi-format-list-bulleted
-    v-btn(icon to='/settings')
+    v-btn(icon, to="/settings")
       v-icon mdi-cog
-    v-btn(icon v-if="notify" @click="toggleNotify")
+    v-btn(icon, v-if="notify", @click="toggleNotify")
       v-icon mdi-bell
-    v-btn(icon v-else @click="toggleNotify")
+    v-btn(icon, v-else, @click="toggleNotify")
       v-icon mdi-bell-off
   v-main
     v-container
@@ -21,30 +21,30 @@ v-app
         keep-alive(include="HomeView")
           component(:is="Component")
 
-  v-img.tomato(src="https://github.com/alsoson/pomodoro/blob/master/src/assets/images/tomato.png?raw=true")
+  v-img.tomato(
+    src="https://github.com/alsoson/pomodoro/blob/master/src/assets/images/tomato.png?raw=true"
+  )
 </template>
 
 <script setup>
-import { storeToRefs } from 'pinia'
-import { useListStore } from '@/stores/list'
-import { onMounted } from "vue"
+import { storeToRefs } from "pinia";
+import { useListStore } from "@/stores/list";
+import { onMounted } from "vue";
 
 onMounted(() => {
-
-  gsap.to('.tomato', {
+  gsap.to(".tomato", {
     duration: 10,
-    rotate:1070,
+    rotate: 1070,
     xPercent: 3730,
     yoyo: true,
-    loop:true,
-    repeat:-1,
-    ease:'easeOut',
+    loop: true,
+    repeat: -1,
+    ease: "easeOut",
   });
-})
-const list = useListStore()
-const { notify } = storeToRefs(list)
-const { toggleNotify } = list
+});
+const list = useListStore();
+const { notify } = storeToRefs(list);
+const { toggleNotify } = list;
 
-
-
+// https://hackmd.io/PkufdiVETgWBLQU1aWxvig?view
 </script>
